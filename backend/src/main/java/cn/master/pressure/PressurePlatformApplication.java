6b8fb22b6457678dd227d4df4e811b3dc5d740bd
lijinglin <the2ndindec@gmail.com>
2025-04-15 11:16:56 +0800

    init repository

---
+++ backend/src/main/java/cn/master/pressure/PressurePlatformApplication.java
@@ -0,0 +1,16 @@
+package cn.master.pressure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 11's papa
+ */
+@SpringBootApplication
+public class PressurePlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PressurePlatformApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/cn/master/pressure/PressurePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.master.pressure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PressurePlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
